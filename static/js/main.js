79d0196281d4d53ab689ee926716d5098644c67e
Evan Pellegrini <evanpelle@gmail.com>
2016-08-01 11:17:54 -0700

    finished modal templates

--- static/js/main.js
+++ static/js/main.js
@@ -62,4 +62,28 @@ jQuery(document).ready(function($) {
     GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
 
 
+
+
 });
+
+
+//(function ($) {
+//    "use strict";
+//    function centerModal() {
+//        alert("im doing shit");
+//        $(this).css('display', 'block');
+//        var $dialog  = $(this).find(".modal-dialog"),
+//            offset       = ($(window).height() - $dialog.height()) / 2,
+//            bottomMargin = parseInt($dialog.css('marginBottom'), 10);
+//
+//        // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
+//        if(offset < bottomMargin) offset = bottomMargin;
+//        if(offset < 0) offset = 0;
+//        $dialog.css("margin-top", offset);
+//    }
+//
+//    $(document).on('show.bs.modal', '.modal', centerModal);
+//    $(window).on("resize", function () {
+//        $('.modal:visible').each(centerModal);
+//    });
+//}(jQuery));
