74d450efb56d4aa8dcc126c58fb45b43836a32a8
Evan Pellegrini <evanpelle@gmail.com>
2017-04-25 19:22:44 -0700

    added get_data method

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/get_data')
+def get_amazon_data():
+    response = requests.get('https://w06sur7dz1.execute-api.us-east-1.amazonaws.com/prod/HabiTrackerUpdate?TableName=HabitTracker')
+    return response.text
+
+# @app.route('/post_user', methods=["Post"])
+# def post_user():
 
 
 if __name__ == '__main__':
