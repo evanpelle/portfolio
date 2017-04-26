0929eb0062f381b1807d690f1b4cd956eb863a86
Evan Pellegrini <evanpelle@gmail.com>
2017-04-26 11:01:02 -0700

    removed IndexIgnore

--- portfolio.py
+++ portfolio.py
@@ -1,22 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 import requests
 
 app = Flask(__name__)
 
+url = 'https://w06sur7dz1.execute-api.us-east-1.amazonaws.com/prod/HabiTrackerUpdate?TableName=HabitTracker'
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
 
-@app.route('/get_data')
+@app.route('/get_data', methods=['GET', 'POST'])
 def get_amazon_data():
-    response = requests.get('https://w06sur7dz1.execute-api.us-east-1.amazonaws.com/prod/HabiTrackerUpdate?TableName=HabitTracker')
-    return response.text
-
-# @app.route('/post_user', methods=["Post"])
-# def post_user():
+    if request.method == 'GET':
+        response = requests.get(url)
+        return response.text
+    else:
+        r = requests.post(url, data=request.data)
+        return r.text
 
 
 if __name__ == '__main__':
